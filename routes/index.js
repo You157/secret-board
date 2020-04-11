d94114457df5644c111c87cd83d382f2efa5f964
You157 <youshimo26@gmail.com>
2020-04-11 15:35:22 +0900

    destroy機能導入ﾃｽﾄ

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const Post = require('../models/post');　// DBを設定したﾌｧｲﾙ
-const crypto = require('crypto');　// ハッシュ関数を利用するためのモジュール
+const Post = require('../models/post');　// DB設定したﾌｧｲﾙ
+const crypto = require('crypto');　// ﾊｯｼｭ関数を利用するためのﾓｼﾞｭｰﾙ
 const oneTimeTokenMap = new Map();　// Key(ﾕｰｻﾞ名)：Value(ﾄｰｸﾝ)とした連想配列
-const moment = require('moment-timezone');　// 投稿Dateを見やすく編集する関数
+const moment = require('moment-timezone');　// Dateを見やすく編集するﾓｼﾞｭｰﾙ
 const trackingIdKey = 'tracking_id';　// cookieのｷｰ
-const secretKey ='cd609e534e6e396261b09336f4a36fd673c5e819f527c7745a8eedfb2bfef366b13e93559cf6173e21721223039f076ee3e76cba5666a8d463c97d123dd75dadb0c40ee8b50af92d804f5c626fe232657f2262bc6a4e0778d44b8e4ea12a42f46ec52895e5d9bc21b2386278a934503b67b3f5becfc4ee562e0966c0213cbc34f7e62aa9ed575f1f2faf43e86e3381f086bf3d19d7840e3f4ab8aafa19436a80f161d841b51fcc74f75d1cf4cb97cc0ff0fb655c990aceeab4f632634342cec3d7b58702a3f94fbff0b18b1fed424e15c9db09f45e24f2cd9b7398362456d3b9dc5bdf107be528e0a93a0d6d5d8cd250fe047f6326631d61479e1155018f984d';
+const secretKey = 'cd609e534e6e396261b09336f4a36fd673c5e819f527c7745a8eedfb2bfef366b13e93559cf6173e21721223039f076ee3e76cba5666a8d463c97d123dd75dadb0c40ee8b50af92d804f5c626fe232657f2262bc6a4e0778d44b8e4ea12a42f46ec52895e5d9bc21b2386278a934503b67b3f5becfc4ee562e0966c0213cbc34f7e62aa9ed575f1f2faf43e86e3381f086bf3d19d7840e3f4ab8aafa19436a80f161d841b51fcc74f75d1cf4cb97cc0ff0fb655c990aceeab4f632634342cec3d7b58702a3f94fbff0b18b1fed424e15c9db09f45e24f2cd9b7398362456d3b9dc5bdf107be528e0a93a0d6d5d8cd250fe047f6326631d61479e1155018f984d';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -40,7 +40,7 @@ router.post('/', (req, res) => {
   const requestedOneTimeToken = req.body.oneTimeToken;
   const requestedUserName = req.user.username;
   if (oneTimeTokenMap.get(requestedUserName) === requestedOneTimeToken) {
-    // req.userとonrTimeTokenの組み合わせが一致する場合
+    // req.userとoneTimeTokenの組み合わせが一致する場合
     Post.create({
       content: req.body.content,
       trackingCookie: req.cookies.tracking_id,
@@ -61,6 +61,30 @@ router.post('/', (req, res) => {
     res.end('未対応のリクエストです');
   }
 });
+
+// post delete req このﾊﾝﾄﾞﾗで取得できるかﾃｽﾄ
+router.post('/?delete=1', (req, res) => {
+  const id = req.body.id;
+  console.log(`☆==--${id}`);
+  res.end();
+  /**
+  Post.findByPk(id).then((post)=>{
+    console.log(`${post.id}:${post.content}`);
+    post.destroy().then(()=>{
+      res.redirect('/');
+    })
+  });
+   */
+});
+
+function deletePost(id, done, err) {
+  Post.findByPk(id).then((post) => {
+    console.log(`${post.id}:${post.content}`);
+    post.destroy().then(()=>{
+      
+    });
+  });
+}
 
 // cookieにtracking_idをｾｯﾄして、console.log用にidをﾘﾀｰﾝする
 function addTrackingCookie(req, res, userName) {
